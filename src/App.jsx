66867367e2221001ee2f25f5da9cfd5cbbc7a102
MariaLight <61028743+MariaLight@users.noreply.github.com>
2025-02-09 17:24:28 +1200

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,23 @@
 import styles from './app.module.css';
-import { useState } from 'react';
-import { useRequestGetTasks } from './hooks/index';
 import { ListItem } from './components/ListItem/ListItem';
 import { ErrorPage } from './components/ErrorPage/ErrorPage';
 import { MainPage } from './components/MainPage/MainPage';
-import { NavLink, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
 
 
 function App() {
-
-
   const [refreshTasksFlag, setRefreshTasksFlag] = useState(false);
   const refreshTasks = () => setRefreshTasksFlag(!refreshTasksFlag);
-  const [warningText, setWarningText] = useState('Задач пока нет');
   const [tasks, setTasks] = useState([]);
-
-  const { isLoading } = useRequestGetTasks(refreshTasksFlag, setTasks, setWarningText);
-
 
   return (
     <div className={styles.app}>
       <Routes>
-        <Route path="/" element={<MainPage refreshTasks={refreshTasks} tasks={tasks} setTasks={setTasks} warningText={warningText} isLoading={isLoading} setWarningText={setWarningText} />}></Route>
-        {
-          tasks.map((item) => <Route key={item.id} path='/task/:id' element={<ListItem tasks={tasks} refreshTasks={refreshTasks} />}></Route>)
-        }
-        <Route path="/404" element={<ErrorPage />}></Route>
-        <Route path="*" element={<ErrorPage />}></Route>
-
+        <Route path="/" element={<MainPage tasks={tasks} setTasks={setTasks} refreshTasks={refreshTasks} setRefreshTasksFlag={setRefreshTasksFlag} />} />
+        <Route path='/task/:id' element={<ListItem tasks={tasks} refreshTasks={refreshTasks} />} />
+        <Route path="/404" element={<ErrorPage />} />
+        <Route path="*" element={<ErrorPage />} />
       </Routes>
 
 
--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -1,4 +1,6 @@
 import styles from '../../app.module.css';
+import { useState } from 'react';
+import { useRequestGetTasks } from '../../hooks/useRequestGetTasks';
 import { Sorting } from '../Sorting/Sorting';
 import { Search } from '../Search/Search';
 import { FormAddTask } from '../FormAddTask/FormAddTask';
@@ -6,7 +8,11 @@ import { NavLink } from 'react-router-dom';
 
 
 
-export const MainPage = ({ tasks, setTasks, refreshTasks, isLoading, warningText, setWarningText }) => {
+export const MainPage = ({tasks, setTasks, refreshTasks, refreshTasksFlag}) => {
+    
+    const [warningText, setWarningText] = useState('Задач пока нет');
+    const { isLoading } = useRequestGetTasks(refreshTasksFlag, setTasks, setWarningText);
+
 
     return (
         <>
@@ -34,7 +40,7 @@ export const MainPage = ({ tasks, setTasks, refreshTasks, isLoading, warningText
                         <div className={styles.loader}></div>
                         :
                         (tasks.length > 0 ?
-                            tasks.map((item) => <li className={`${styles.nav__link} ${item.completed ? styles.link_checked : "" }`} key={item.id}><NavLink to={`task/${item.id}`}>{item.title}</NavLink></li>)
+                            tasks.map((item) => <li className={`${styles.nav__link} ${item.completed ? styles.link_checked : ""}`} key={item.id}><NavLink to={`task/${item.id}`}>{item.title}</NavLink></li>)
                             :
                             <div className={styles.emptyList}>
                                 {warningText}
